5e54c982c3a82ebf53252ea43c80386313a93819
Sabīne Caunīte <a240352sc@rvt.lv>
2025-09-09 13:43:32 +0300

    java programmas

---
+++ src/main/java/rvt/Program.java
@@ -0,0 +1,20 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        //1
+        Scanner scanner = new Scanner(System.in);
+
+        //2
+        System.out.println("Write a message: ");
+
+        //3
+        String message = scanner.nextLine();
+
+        // 4
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/rvt/message.java
@@ -0,0 +1,12 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class message {
+    public static void main(String[]args) {
+        Scanner reader = new Scanner(System.in);
+        System.out .println(x:"Write a message: ");
+        String input = reader.nextLine();
+        System.out.println(input);
+    }
+}
