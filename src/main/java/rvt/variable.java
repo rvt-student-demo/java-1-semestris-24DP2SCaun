80733811deb5fa5c2917395bc7befb1537894c85
Sabīne Caunīte <a240352sc@rvt.lv>
2025-09-10 13:07:31 +0300

    various variables

---
+++ src/main/java/rvt/variable.java
@@ -0,0 +1,26 @@
+package rvt;
+
+public class variable {
+
+    public static void main(String[] args) {
+
+        int chicken = 9000;
+        double bacon = 0.1;
+        String tractor = "Zator";
+
+
+        System.out.println("Chicken: ");
+        System.out.println(chicken);
+        System.out.println("Bacon(kg): ");
+        System.out.println(bacon);
+        System.out.println("Tractor: ");
+        System.out.println(tractor);
+        System.out.println();
+        System.out.println("And finally, a summary: ");
+        System.out.println(chicken);
+        System.out.println(bacon);
+        System.out.println(tractor);
+    
+    }
+
+}
