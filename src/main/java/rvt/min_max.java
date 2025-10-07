7fb60c61a697eabf4b8a3dd549ed50e329220a0c
Sabīne Caunīte <a240352sc@rvt.lv>
2025-10-07 11:12:38 +0300

    min un max teorija

---
+++ src/main/java/rvt/min_max.java
@@ -0,0 +1,19 @@
+package rvt;
+
+
+public class min_max {
+public static void main(String[] args) {
+        int min = 5;
+        int max = 10;
+
+        MusuMetode(min, max);
+        System.out.println(min + " : " + max);
+
+    }
+
+    public static void MusuMetode(int min, int max) {
+        min = 1; max = 5;
+    
+    }
+
+}
