a5f5d96b1dd6cdea8d7569453504acb5c883526a
Sabīne Caunīte <a240352sc@rvt.lv>
2025-10-14 12:24:04 +0300

    saskaitišana cikls procentu darbs ar blokshēmu

---
+++ src/main/java/rvt/reizinasana_cikls.java
@@ -0,0 +1,16 @@
+package rvt;
+
+
+public class reizinasana_cikls {
+
+    public static void main(String[] args) {
+        
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+            System.out.println(sum);
+        }
+    }
+}
