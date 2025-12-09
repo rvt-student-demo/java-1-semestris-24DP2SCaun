34d1eb41f5475ca8eed2287695bd83eceaa60c42
Sabīne Caunīte <A240352SC@rvt.lv>
2025-12-09 23:22:30 +0200

    divisible by three

---
+++ src/main/java/rvt/divisible_by_three.java
@@ -0,0 +1,19 @@
+package rvt;
+
+
+public class divisible_by_three {
+    public class DivisibleByThree {
+
+    public static void divisibleByThreeInRange(int beginning, int end){
+        while(beginning <= end){
+            if(beginning % 3 == 0){
+                System.out.println(beginning);
+            } else {
+                beginning++;
+                continue;
+            }
+            beginning += 3;
+        }
+    } 
+}
+}
