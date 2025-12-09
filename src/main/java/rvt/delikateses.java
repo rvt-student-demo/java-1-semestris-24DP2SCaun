a0cac41f70dd0320a51cd557be75409942d405d5
Sabīne Caunīte <A240352SC@rvt.lv>
2025-12-09 22:31:03 +0200

    delikateses

---
+++ src/main/java/rvt/delikateses.java
@@ -0,0 +1,39 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class delikateses {
+
+
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+System.out.print("Ievadi preci: ");
+String prece = scanner.nextLine();
+
+System.out.print("Ievadi cenu: ");
+double cena = scanner.nextDouble();
+
+System.out.print("Ekspress piegade (0=ne, 1=ja): ");
+int express = scanner.nextInt();
+
+double piegade = 0;
+
+if (cena < 10) {
+piegade = 2;
+}
+
+if (express == 1) {
+piegade += 3;
+}
+
+double kopa = cena + piegade;
+
+System.out.println("\nRekins:");
+System.out.println(prece + " " + cena);
+System.out.println("piegade " + piegade);
+System.out.println("kopa " + kopa);
+scanner.close();
+}
+
+}
