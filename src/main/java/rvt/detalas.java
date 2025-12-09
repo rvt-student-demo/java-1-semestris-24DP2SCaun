2a86cd2158b069bfa93aabec72940afe8db6789f
Sabīne Caunīte <A240352SC@rvt.lv>
2025-12-09 22:29:04 +0200

    detalas

---
+++ src/main/java/rvt/detalas.java
@@ -0,0 +1,45 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class detalas {
+
+    public static void main(String[] args) {
+        
+        final int skruves_cena = 5;
+        final int uzgriezna_cena = 3;
+        final int paplaksna_cena = 1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ievadiet skruvju skaitu: ");
+        int skruves = scanner.nextInt();
+
+        System.out.print("Ievadiet uzgrieznu skaitu: ");
+        int uzgriezni = scanner.nextInt();
+
+        System.out.print("Ievadiet paplaksnu skaitu: ");
+        int paplaksnes = scanner.nextInt();
+
+        boolean irKluda = false;
+        
+        if (uzgriezni < skruves) {
+            System.out.println("Parbaudi pasutijumu: par maz uzgrieznu");
+            irKluda = true;
+        }
+
+        if (paplaksnes < 2 * skruves) {
+            System.out.println("Parbaudi pasutijumu: par maz paplaksnu");
+            irKluda = true;
+        }
+
+        if (!irKluda) {
+            System.out.println("Pasutijums ir kartiba.");
+        }
+
+        int kopejaCena = (skruves * skruves_cena) + (uzgriezni * uzgriezna_cena) + (paplaksnes * paplaksna_cena);
+        System.out.println("Kopeja cena: " + kopejaCena);
+
+        scanner.close();
+    }
+}
